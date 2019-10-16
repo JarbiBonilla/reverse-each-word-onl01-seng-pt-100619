0943f8db0e88e8c289c5a091d3983c9d31c16c0c
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 23:22:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(sentence1)
   reversed = []
-  sentence_array = sentence1.split(" ")
+  sentence_array = sentence1.split("")
   sentence_array.each do |word|
     reversed << word.reverse
     #binding.pry
