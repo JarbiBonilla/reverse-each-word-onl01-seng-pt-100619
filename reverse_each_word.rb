c02ccbacc885b519187af7f0d46bb1cb3e05ba04
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-16 23:22:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,8 @@ def reverse_each_word(sentence1)
   reversed = []
   sentence_array = sentence1.split(" ")
   sentence_array.each do |word|
-    
+    reversed << word.reverse
     #binding.pry
 end
+return reversed
 end
